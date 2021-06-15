dc056ffadce4c2f446094a9b4dd94069460c2305
张亚飞 <672233580@qq.com>
2021-06-15 09:16:31 +0800

    Initial Commit

---
+++ TwitterProfileScrolling/TwitterProfileScrollingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterProfileScrollingApp.swift
+//  TwitterProfileScrolling
+//
+//  Created by 张亚飞 on 2021/6/15.
+//
+
+import SwiftUI
+
+@main
+struct TwitterProfileScrollingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
